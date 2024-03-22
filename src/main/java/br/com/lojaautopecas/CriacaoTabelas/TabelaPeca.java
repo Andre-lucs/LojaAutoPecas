3320185ba3213cbf79d5e724a2b27fab2e4cd67b
José Vieira da Silva Neto <josevieira2029@gmail.com>
2024-03-22 20:53:57 -0300

    CRUD PecaDao + SQL Tabela PecaDao

--- src/main/java/br/com/lojaautopecas/CriacaoTabelas/TabelaPeca.java
+++ src/main/java/br/com/lojaautopecas/CriacaoTabelas/TabelaPeca.java
@@ -1,5 +1,27 @@
 package br.com.lojaautopecas.CriacaoTabelas;
 
-public class TabelaPeca {
+import br.com.lojaautopecas.jdbc.DBConnection;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TabelaPeca {
+    private Connection conexao;
+
+    public TabelaPeca() {
+        this.conexao = new DBConnection().getConexao();
+    }
+
+    public void criar() throws SQLException {
+        // SQL para criar a tabela Cliente
+        String sql = "CREATE TABLE peca (" +
+                "id SERIAL PRIMARY KEY," +
+                "nome VARCHAR(100) NOT NULL," +
+                "preco DOUBLE PRECISION)";
+        Statement stmt = conexao.createStatement();
+        stmt.execute(sql);
+        System.out.println("Tabela Peça criada com sucesso!");
+        conexao.close();
+    }
 }
--- src/main/java/br/com/lojaautopecas/dao/PecaDao.java
+++ src/main/java/br/com/lojaautopecas/dao/PecaDao.java
@@ -1,4 +1,116 @@
 package br.com.lojaautopecas.dao;
 
+import br.com.lojaautopecas.CriacaoTabelas.TabelaPeca;
+import br.com.lojaautopecas.jdbc.DBConnection;
+import br.com.lojaautopecas.model.Peca;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PecaDao {
-}
+
+    private Connection conexao;
+
+    public PecaDao() throws SQLException {
+        this.conexao = new DBConnection().getConexao();
+    }
+
+    private boolean tabelaPecaExiste() throws SQLException {
+        DatabaseMetaData metaData = conexao.getMetaData();
+        ResultSet tables = metaData.getTables(null, null, "peca", null);
+        return tables.next();
+    }
+
+    public void inserirPeca(Peca peca) throws SQLException {
+
+        if (!tabelaPecaExiste()) {
+            TabelaPeca tabelaLocacao = new TabelaPeca();
+            tabelaLocacao.criar();
+        }
+
+        String sql = "INSERT INTO peca (nome, preco) VALUES (?, ?)";
+
+        try {
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, peca.getNome());
+            stmt.setDouble(2, peca.getPreco());
+            stmt.execute();
+            stmt.close();
+
+            System.out.println("Peça criada com sucesso!");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Peca> listarPeca() {
+        try {
+            List<Peca> pecas = new ArrayList<>();
+            String sql = "SELECT * FROM peca ORDER BY id";
+
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Peca peca = new Peca();
+                peca.setId(rs.getInt("id"));
+                peca.setNome(rs.getString("nome"));
+                peca.setPreco(rs.getDouble("preco"));
+                pecas.add(peca);
+                System.out.println(peca.getNome());
+            }
+
+            rs.close();
+            stmt.close();
+            return pecas;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void atualizarPeca(int id, Peca novosDadosPeca) {
+        String sql = "UPDATE peca SET nome=?, preco=? WHERE id=?";
+        try {
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, novosDadosPeca.getNome());
+            stmt.setDouble(2, novosDadosPeca.getPreco());
+            stmt.setInt(3, id);
+            stmt.executeUpdate();
+            stmt.close();
+            System.out.println("Peca atualizada com sucesso!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void deletarPeca(int id) {
+        String sql = "DELETE FROM peca WHERE id=?";
+        try {
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            stmt.close();
+            System.out.println("Peca excluída com sucesso!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Peca peca = new Peca();
+
+//        String nomePecaAntiga = "Carburador";
+        peca.setNome("Filtro swww");
+        peca.setPreco(11.32);
+
+        PecaDao pecaDao = new PecaDao();
+
+        pecaDao.inserirPeca(peca);
+        pecaDao.listarPeca();
+//        pecaDao.atualizaPeca(peca, nomePecaAntiga);
+
+
+    }}
